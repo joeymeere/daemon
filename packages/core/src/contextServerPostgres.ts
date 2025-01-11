d9f8d4dc56afc820d16d6a2031bcd0755fc98ecc
spacemandev <devbharel@gmail.com>
2025-01-11 00:07:31 -0600

    contextServerPostgres

--- packages/core/src/contextServerPostgres.ts
+++ packages/core/src/contextServerPostgres.ts
@@ -1,18 +1,21 @@
 import { FastMCP } from "fastmcp";
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import { Pool, type PoolConfig } from "pg";
-import type {
-  Character,
-  IContextServer,
-  IDaemon,
-  ILog,
-  IMemory,
+import {
+  type Approval,
+  type Character,
+  type IContextServer,
+  type ILog,
+  type IMemory,
 } from "./types";
 import { eq, desc, asc, and, cosineDistance, gte } from "drizzle-orm";
 import { pgTable, timestamp } from "drizzle-orm/pg-core";
 import { jsonb, text, uuid, vector } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 import { nanoid } from "nanoid";
+import { z } from "zod";
+import { PublicKey } from "@solana/web3.js";
+import nacl from "tweetnacl";
 
 /**
  * Context Server manages
@@ -24,6 +27,8 @@ import { nanoid } from "nanoid";
 export class ContextServerPostgres implements IContextServer {
   private db: NodePgDatabase<typeof ContextServerSchema>;
   private poolConfig: PoolConfig;
+  private server: FastMCP;
+
   private embeddingInfo: {
     model: string;
     dimensions: number;
@@ -32,22 +37,23 @@ export class ContextServerPostgres implements IContextServer {
 
   constructor(
     pgOpts: PoolConfig,
+    serverOpts?: { name?: string; port?: number },
     embeddingOpts?: { embeddingModel: string; embeddingDimensions: number }
   ) {
     this.poolConfig = pgOpts;
-    this.db = drizzle(
-      {
-        client: new Pool(pgOpts),
-        schema: ContextServerSchema,
-      },
-      {
-        casing: "snake_case",
-      }
-    );
+    this.db = drizzle(new Pool(pgOpts), {
+      schema: ContextServerSchema,
+      casing: "snake_case",
+    });
     this.embeddingInfo = {
       model: embeddingOpts?.embeddingModel || "openai",
       dimensions: embeddingOpts?.embeddingDimensions || 1536,
     };
+
+    this.server = new FastMCP({
+      name: serverOpts?.name || "Daemon Context Server",
+      version: "0.0.1",
+    });
   }
 
   async init(opts?: {
@@ -153,17 +159,140 @@ export class ContextServerPostgres implements IContextServer {
     }
 
     // Reinitialize db instance with the updated Memories Schema
-    this.db = drizzle(
-      {
-        client: new Pool(this.poolConfig),
-        schema: updatedMemoriesWithEmbedding,
-      },
-      {
-        casing: "snake_case",
-      }
-    );
+    this.db = drizzle(new Pool(this.poolConfig), {
+      schema: ContextServerSchema,
+      casing: "snake_case",
+    });
 
     this.initialized = true;
+  }
+
+  async start(port?: number): Promise<void> {
+    if (!this.initialized) {
+      throw new Error("Context Server not initialized");
+    }
+
+    // Server Tools
+    this.server.addTool({
+      name: "registerCharacter",
+      description: "Register a new character",
+      parameters: z.object({
+        name: z.string(),
+        bio: z.array(z.string()),
+        lore: z.array(z.string()),
+        pubkey: z.string(),
+        contextServer: z.string(),
+      }),
+      execute: async (character: Character) => {
+        return await this.registerCharacter(character);
+      },
+    });
+
+    this.server.addTool({
+      name: "fetchCharacter",
+      description: "Fetch a character",
+      parameters: z.object({
+        daemonId: z.string(),
+      }),
+      execute: async (args: { daemonId: string }) => {
+        return {
+          type: "text",
+          text: JSON.stringify(
+            (await this.fetchCharacter(args.daemonId)) || {}
+          ),
+        };
+      },
+    });
+
+    this.server.addTool({
+      name: "fetchLogs",
+      description: "Fetch logs",
+      parameters: z.object({
+        daemonId: z.string(),
+        channelId: z.string().optional(),
+        limit: z.number().optional(),
+        orderBy: z.enum(["asc", "desc"]).optional(),
+      }),
+      execute: async (args) => {
+        return {
+          type: "text",
+          text: JSON.stringify(await this.fetchLogs(args)),
+        };
+      },
+    });
+
+    this.server.addTool({
+      name: "createMemory",
+      description: "Create a memory",
+      parameters: z.object({
+        opts: z.object({
+          daemonId: z.string(),
+          message: z.string(),
+          messageEmbedding: z.array(z.number()),
+          channelId: z.string().optional(),
+          originationLogIds: z.array(z.string()).optional(),
+        }),
+        approval: z.object({
+          signature: z.string(),
+        }),
+      }),
+      execute: async (args) => {
+        return await this.createMemory(args.opts, args.approval);
+      },
+    });
+
+    this.server.addTool({
+      name: "fetchMemoryContext",
+      description: "Fetch memory context",
+      parameters: z.object({
+        daemonId: z.string(),
+        messageEmbedding: z.array(z.number()),
+        limit: z.number(),
+        similarityThreshold: z.number().optional(),
+        channelId: z.string().optional(),
+      }),
+      execute: async (args) => {
+        return {
+          type: "text",
+          text: JSON.stringify(await this.fetchMemoryContext(args)),
+        };
+      },
+    });
+
+    this.server.addTool({
+      name: "createLog",
+      description: "Insert a log",
+      parameters: z.object({
+        log: z.object({
+          daemonId: z.string(),
+          channelId: z.string().nullable(),
+          content: z.string(),
+          logType: z.enum(["input", "output"]),
+        }),
+        approval: z.object({
+          signature: z.string(),
+        }),
+      }),
+      execute: async (args) => {
+        return {
+          type: "text",
+          text: JSON.stringify(await this.createLog(args.log, args.approval)),
+        };
+      },
+    });
+
+    // Start the Server
+    this.server.start({
+      transportType: "sse",
+      sse: {
+        endpoint: `/sse`,
+        port: port || 8080,
+      },
+    });
+  }
+
+  async stop(): Promise<void> {
+    await this.server.stop();
   }
 
   async registerCharacter(character: Character): Promise<string> {
@@ -221,36 +350,6 @@ export class ContextServerPostgres implements IContextServer {
     }));
   }
 
-  async createMemory(opts: {
-    daemonId: string;
-    message: string;
-    messageEmbedding: number[];
-    channelId?: string;
-    originationLogIds?: string[];
-  }): Promise<string> {
-    if (!this.initialized) {
-      throw new Error("Context Server not initialized");
-    }
-
-    if (opts.messageEmbedding.length !== this.embeddingInfo.dimensions) {
-      throw new Error(
-        `Embedding dimensions mismatch. Expected ${this.embeddingInfo.dimensions}, got ${opts.messageEmbedding.length}`
-      );
-    }
-
-    const memoryId = nanoid();
-    await this.db.insert(ContextServerSchema.memories).values({
-      id: memoryId,
-      daemonId: opts.daemonId,
-      channelId: opts.channelId,
-      createdAt: new Date(),
-      content: opts.message,
-      embedding: opts.messageEmbedding,
-      originationLogIds: opts.originationLogIds || [],
-    });
-    return memoryId;
-  }
-
   async fetchMemoryContext(opts: {
     daemonId: string;
     messageEmbedding: number[];
@@ -288,14 +387,93 @@ export class ContextServerPostgres implements IContextServer {
     return relevantMemories;
   }
 
-  async insertLog(opts: { log: ILog }): Promise<void> {
+  async createLog(log: ILog, approval: Approval): Promise<void> {
+    if (!this.initialized) {
+      throw new Error("Context Server not initialized");
+    }
+
+    // Fetch Daemon Pubkey
+    const daemon = await this.db
+      .select({
+        pubkey: ContextServerSchema.daemons.pubkey,
+      })
+      .from(ContextServerSchema.daemons)
+      .where(eq(ContextServerSchema.daemons.id, log.daemonId))
+      .execute();
+    const daemonPubkey = daemon[0]?.pubkey;
+
+    if (!checkApproval(log, daemonPubkey, approval)) {
+      throw new Error("Approval failed");
+    }
+
     await this.db.insert(ContextServerSchema.logs).values({
-      id: opts.log.id || nanoid(),
-      ...opts.log,
+      id: log.id || nanoid(),
+      ...log,
       createdAt: new Date(),
     });
   }
+
+  async createMemory(
+    opts: {
+      daemonId: string;
+      message: string;
+      messageEmbedding: number[];
+      channelId?: string;
+      originationLogIds?: string[];
+    },
+    approval: Approval
+  ): Promise<string> {
+    if (!this.initialized) {
+      throw new Error("Context Server not initialized");
+    }
+
+    if (opts.messageEmbedding.length !== this.embeddingInfo.dimensions) {
+      throw new Error(
+        `Embedding dimensions mismatch. Expected ${this.embeddingInfo.dimensions}, got ${opts.messageEmbedding.length}`
+      );
+    }
+
+    // Fetch Daemon Pubkey
+    const daemon = await this.db
+      .select({
+        pubkey: ContextServerSchema.daemons.pubkey,
+      })
+      .from(ContextServerSchema.daemons)
+      .where(eq(ContextServerSchema.daemons.id, opts.daemonId))
+      .execute();
+    const daemonPubkey = daemon[0]?.pubkey;
+
+    if (!checkApproval(opts, daemonPubkey, approval)) {
+      throw new Error("Approval failed");
+    }
+
+    const memoryId = nanoid();
+    await this.db.insert(ContextServerSchema.memories).values({
+      id: memoryId,
+      daemonId: opts.daemonId,
+      channelId: opts.channelId,
+      createdAt: new Date(),
+      content: opts.message,
+      embedding: opts.messageEmbedding,
+      originationLogIds: opts.originationLogIds || [],
+    });
+    return memoryId;
+  }
 }
+
+const checkApproval = (opts: any, publicKey: string, approval: Approval) => {
+  const hasher = new Bun.CryptoHasher("sha256");
+  const message = JSON.stringify(opts, null, 0);
+  hasher.update(message);
+  const hash = hasher.digest("hex");
+  const pubkey = new PublicKey(publicKey);
+
+  return nacl.sign.detached.verify(
+    Buffer.from(hash, "hex"),
+    Buffer.from(approval.signature, "base64"),
+    pubkey.toBytes()
+  );
+};
 
 const settings = {
   key: text("key").primaryKey(),
--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -9,15 +9,9 @@ export interface IDaemon {
 
 export type Character = {
   name: string;
-  // Bio is how the character acts (personality)
   bio: string[];
-  // Lore is what the character has been involved with (historical events)
   lore: string[];
-  // Pubkey of the character
   pubkey: string;
-
-  // Context Server URL (MCP Server)
-  // Used to store Common Memories and Channel Memories
   contextServer: string;
 };
 
@@ -39,30 +33,25 @@ export interface ILog {
   daemonId: string;
   // Optionally scoped to just a private conversation between human and daemon
   channelId: string | null;
-  createdAt: Date;
+  createdAt?: Date;
   content: string;
   logType: "input" | "output";
 }
 
 export interface Approval {
-  hash: string; // sha256 hash of opts
   signature: string; //b64 signature of the hash
 }
 
 export interface IContextServer {
   init(): Promise<void>;
+  start(): Promise<void>;
 
   // Register New Character (returns characterId)
   registerCharacter(character: Character): Promise<string>;
   // Fetch Character File
   fetchCharacter(characterId: string): Promise<Character | undefined>;
   // Insert Log
-  insertLog(
-    opts: {
-      log: ILog;
-    },
-    approval: Approval
-  ): Promise<void>;
+  createLog(log: ILog, approval: Approval): Promise<void>;
   // Fetch Logs
   fetchLogs(opts: {
     daemonId: string;
